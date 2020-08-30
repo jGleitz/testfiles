8f504a3a190d117c84a705f6a5698f55581a64bb
Joshua Gleitze <dev@joshuagleitze.de>
2020-08-30 20:52:50 +0200

    fix: remove usage of Java APIs from after Java 8

--- src/test/kotlin/util.kt
+++ src/test/kotlin/util.kt
@@ -5,11 +5,11 @@ import io.mockk.mockk
 import org.spekframework.spek2.runtime.scope.ScopeId
 import org.spekframework.spek2.runtime.scope.ScopeImpl
 import org.spekframework.spek2.runtime.scope.ScopeType
-import java.nio.file.Files.readString
+import java.nio.file.Files.readAllBytes
 import java.nio.file.Path
 
-internal inline fun <reified S: ScopeImpl> mockScope(name: String) = mockk<S>().also {
-	every { it.id } returns ScopeId(ScopeType.Scope, name)
+internal inline fun <reified S : ScopeImpl> mockScope(name: String) = mockk<S>().also {
+    every { it.id } returns ScopeId(ScopeType.Scope, name)
 }
 
-internal val Expect<Path>.content get() = feature("string content") { readString(this) }
+internal val Expect<Path>.content get() = feature("string content") { String(readAllBytes(this)) }
