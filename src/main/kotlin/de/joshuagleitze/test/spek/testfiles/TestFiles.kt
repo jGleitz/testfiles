08e8a05fe8861d64df959d65bdf2a7c1a67a17f3
Joshua Gleitze <dev@joshuagleitze.de>
2020-10-12 19:27:42 +0200

    fix: make returned paths absolute (#29)

--- src/main/kotlin/de/joshuagleitze/test/spek/testfiles/TestFiles.kt
+++ src/main/kotlin/de/joshuagleitze/test/spek/testfiles/TestFiles.kt
@@ -6,7 +6,9 @@ import java.nio.file.Path
 /**
  * Helper that creates test files and directories for use in tests. The helper manages a directory structures that reflects the structure of
  * the Spek tests. Created files and directories will reside in the part of the directory structure that corresponds to the current Spek
- * group or test. The helper also manages when to delete the created files, see [DeletionMode].
+ * group or test. The helper also manages when to delete the created files.
+ *
+ * @see DeletionMode
  */
 interface TestFiles {
 	/**
@@ -14,6 +16,7 @@ interface TestFiles {
 	 *
 	 * @param name The name of the directory. If omitted or `null`, the name will be generated.
 	 * @param delete When to delete the created directory, see [DeletionMode].
+	 * @return The absolute [Path] to the created directory.
 	 */
 	fun createDirectory(name: String? = null, delete: DeletionMode = IF_SUCCESSFUL): Path
 
@@ -22,6 +25,7 @@ interface TestFiles {
 	 *
 	 * @param name The name of the file. If omitted or `null`, the name will be generated.
 	 * @param delete When to delete the created file, see [DeletionMode].
+	 * @return The absolute [Path] to the created file.
 	 */
 	fun createFile(name: String? = null, delete: DeletionMode = IF_SUCCESSFUL): Path
 }
