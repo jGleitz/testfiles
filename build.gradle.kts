3b7d4a70073a70f430b758603cd10341c83c4646
Joshua Gleitze <dev@joshuagleitze.de>
2021-01-03 21:03:22 +0100

    build: restructure into main project and subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,28 +18,33 @@ group = "de.joshuagleitze"
 version = if (isSnapshot) versionDetails.gitHash else versionDetails.lastTag.drop("v")
 status = if (isSnapshot) "snapshot" else "release"
 
-repositories {
-	jcenter()
+allprojects {
+	repositories {
+		jcenter()
+	}
 }
 
 dependencies {
-	val spekVersion = "2.0.15"
-
-	// Spek is a peer dependency which is not declared by this module
-	compileOnly(name = "spek-dsl-jvm", group = "org.spekframework.spek2", version = spekVersion)
-	compileOnly(name = "spek-runtime-jvm", group = "org.spekframework.spek2", version = spekVersion)
-
-	testImplementation(name = "spek-dsl-jvm", version = spekVersion, group = "org.spekframework.spek2")
-	testImplementation(name = "spek-runtime-jvm", group = "org.spekframework.spek2", version = spekVersion)
+	testImplementation(name = "spek-dsl-jvm", version = "2.0.15", group = "org.spekframework.spek2")
+	testImplementation(name = "spek-runtime-jvm", group = "org.spekframework.spek2", version = "2.0.15")
 	testImplementation(name = "atrium-fluent-en_GB", version = "0.15.0", group = "ch.tutteli.atrium")
 	testImplementation(name = "niok", version = "1.3.4", group = "ch.tutteli.niok")
 	testImplementation(name = "mockk", version = "1.10.4", group = "io.mockk")
-	testRuntimeOnly(name = "spek-runner-junit5", version = spekVersion, group = "org.spekframework.spek2")
+	testRuntimeOnly(name = "spek-runner-junit5", version = "2.0.15", group = "org.spekframework.spek2")
 
 	constraints {
-		testImplementation("org.jetbrains.kotlin:kotlin-reflect:${KotlinCompilerVersion.VERSION}") {
-			because("transitive dependencies refer to previous versions, but all Kotlin artefacts need to have the same version")
-		}
+		testImplementation(kotlin("reflect", version = KotlinCompilerVersion.VERSION))
+	}
+}
+
+java {
+	sourceCompatibility = VERSION_1_8
+	targetCompatibility = VERSION_1_8
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		jvmTarget = "1.8"
 	}
 }
 
@@ -54,116 +59,11 @@ tasks.withType<Test> {
 	workingDir = testPwd
 }
 
-java {
-	sourceCompatibility = VERSION_1_8
-	targetCompatibility = VERSION_1_8
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		jvmTarget = "1.8"
-	}
-}
-
 val ossrhUsername: String? by project
 val ossrhPassword: String? by project
 val githubRepository: String? by project
 val githubOwner = githubRepository?.split("/")?.get(0)
 val githubToken: String? by project
-
-val sourcesJar by tasks.creating(Jar::class) {
-	group = "build"
-	description = "Assembles the source code into a jar"
-	archiveClassifier.set("sources")
-	from(sourceSets.main.get().allSource)
-}
-
-tasks.withType<DokkaTask> {
-	dokkaSourceSets.named("main") {
-		sourceLink {
-			localDirectory.set(file("src/main/kotlin"))
-			remoteUrl.set(uri("https://github.com/$githubRepository/blob/master/src/main/kotlin").toURL())
-			remoteLineSuffix.set("#L")
-		}
-	}
-}
-
-val dokkaJar by tasks.creating(Jar::class) {
-	group = "build"
-	description = "Assembles the Kotlin docs with Dokka"
-	archiveClassifier.set("javadoc")
-	from(tasks.named("dokkaJavadoc"))
-}
-
-artifacts {
-	archives(sourcesJar)
-	archives(dokkaJar)
-}
-
-lateinit var publication: MavenPublication
-lateinit var githubPackages: ArtifactRepository
-lateinit var mavenCentral: NexusRepository
-
-publishing {
-	publications {
-		publication = create<MavenPublication>("maven") {
-			from(components["java"])
-			artifact(sourcesJar)
-			artifact(dokkaJar)
-
-			pom {
-				name.set(provider { "$groupId:$artifactId" })
-				description.set("Easily manage test files and directories when testing with Spek!")
-				inceptionYear.set("2020")
-				url.set("https://github.com/$githubRepository")
-				ciManagement {
-					system.set("GitHub Actions")
-					url.set("https://github.com/$githubRepository/actions")
-				}
-				issueManagement {
-					system.set("GitHub Issues")
-					url.set("https://github.com/$githubRepository/issues")
-				}
-				developers {
-					developer {
-						name.set("Joshua Gleitze")
-						email.set("dev@joshuagleitze.de")
-					}
-				}
-				scm {
-					connection.set("scm:git:https://github.com/$githubRepository.git")
-					developerConnection.set("scm:git:git://git@github.com:$githubRepository.git")
-					url.set("https://github.com/$githubRepository")
-				}
-				licenses {
-					license {
-						name.set("MIT")
-						url.set("https://opensource.org/licenses/MIT")
-						distribution.set("repo")
-					}
-				}
-			}
-		}
-	}
-	repositories {
-		githubPackages = maven("https://maven.pkg.github.com/$githubRepository") {
-			name = "GitHubPackages"
-			credentials {
-				username = githubOwner
-				password = githubToken
-			}
-		}
-	}
-}
-
-nexusPublishing {
-	repositories {
-		mavenCentral = sonatype {
-			username.set(ossrhUsername)
-			password.set(ossrhPassword)
-		}
-	}
-}
 
 nexusStaging {
 	username = ossrhUsername
@@ -171,17 +71,104 @@ nexusStaging {
 	numberOfRetries = 42
 }
 
+project.evaluationDependsOnChildren()
+
+val closeAndReleaseRepository by project.tasks
+
+allprojects {
+	val sourcesJar by tasks.registering(Jar::class) {
+		group = "build"
+		description = "Assembles the source code into a jar"
+		archiveClassifier.set("sources")
+		from(sourceSets.main.map { it.allSource })
+	}
+
+	tasks.withType<DokkaTask> {
+		dokkaSourceSets.named("main") {
+			sourceLink {
+				localDirectory.set(file("src/main/kotlin"))
+				remoteUrl.set(uri("https://github.com/$githubRepository/blob/master/src/main/kotlin").toURL())
+				remoteLineSuffix.set("#L")
+			}
+		}
+	}
+
+	val dokkaJar by tasks.registering(Jar::class) {
+		group = "build"
+		description = "Assembles the Kotlin docs with Dokka"
+		archiveClassifier.set("javadoc")
+		from(tasks.named("dokkaJavadoc"))
+	}
+
+	artifacts {
+		archives(sourcesJar)
+		archives(dokkaJar)
+	}
+
+	rootProject.publishing.publications.register<MavenPublication>(name) {
+		from(components["java"])
+		artifact(sourcesJar)
+		artifact(dokkaJar)
+
+		rootProject.signing.sign(this)
+
+		pom {
+			name.set(provider { "$groupId:$artifactId" })
+			description.set("Easily manage test files and directories when testing with Spek!")
+			inceptionYear.set("2020")
+			url.set("https://github.com/$githubRepository")
+			ciManagement {
+				system.set("GitHub Actions")
+				url.set("https://github.com/$githubRepository/actions")
+			}
+			issueManagement {
+				system.set("GitHub Issues")
+				url.set("https://github.com/$githubRepository/issues")
+			}
+			developers {
+				developer {
+					name.set("Joshua Gleitze")
+					email.set("dev@joshuagleitze.de")
+				}
+			}
+			scm {
+				connection.set("scm:git:https://github.com/$githubRepository.git")
+				developerConnection.set("scm:git:git://git@github.com:$githubRepository.git")
+				url.set("https://github.com/$githubRepository")
+			}
+			licenses {
+				license {
+					name.set("MIT")
+					url.set("https://opensource.org/licenses/MIT")
+					distribution.set("repo")
+				}
+			}
+		}
+	}
+}
+
+val githubPackages = publishing.repositories.maven("https://maven.pkg.github.com/$githubRepository") {
+	name = "GitHubPackages"
+	credentials {
+		username = githubOwner
+		password = githubToken
+	}
+}
+
+val mavenCentral = nexusPublishing.repositories.sonatype {
+	username.set(ossrhUsername)
+	password.set(ossrhPassword)
+}
+
 signing {
 	val signingKey: String? by project
 	val signingKeyPassword: String? by project
 	useInMemoryPgpKeys(signingKey, signingKeyPassword)
-	sign(publication)
 }
 
-val closeAndReleaseRepository by project.tasks
 closeAndReleaseRepository.mustRunAfter(mavenCentral.publishTask)
 
-task("release") {
+tasks.register("release") {
 	group = "release"
 	description = "Releases the project to all remote repositories"
 	dependsOn(githubPackages.publishTask, mavenCentral.publishTask, closeAndReleaseRepository)
