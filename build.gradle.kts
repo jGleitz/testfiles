1899712d07b8068ea938984d0ac8516d74af01da
Joshua Gleitze <dev@joshuagleitze.de>
2021-01-04 03:41:28 +0100

    build: configure releasing in afterEvaluate

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,120 +43,121 @@ nexusStaging {
 	numberOfRetries = 42
 }
 
-evaluationDependsOnChildren()
 val closeAndReleaseRepository by project.tasks
 
 subprojects {
-	apply {
-		plugin("org.jetbrains.dokka")
-		plugin("de.marcphilipp.nexus-publish")
-		plugin("org.gradle.maven-publish")
-		plugin("org.gradle.signing")
-	}
-
-	val sourcesJar by tasks.registering(Jar::class) {
-		group = "build"
-		description = "Assembles the source code into a jar"
-		archiveClassifier.set("sources")
-		from(sourceSets.main.map { it.allSource })
-	}
-
-	tasks.withType<DokkaTask> {
-		dokkaSourceSets.named("main") {
-			this.DokkaSourceSetID(if (extra.has("artifactId")) extra["artifactId"] as String else project.name)
-			sourceLink {
-				val projectPath = projectDir.absoluteFile.relativeTo(rootProject.projectDir.absoluteFile)
-				localDirectory.set(file("src/main/kotlin"))
-				remoteUrl.set(uri("https://github.com/$githubRepository/blob/$gitRef/$projectPath/src/main/kotlin").toURL())
-				remoteLineSuffix.set("#L")
-			}
+	afterEvaluate {
+		apply {
+			plugin("org.jetbrains.dokka")
+			plugin("de.marcphilipp.nexus-publish")
+			plugin("org.gradle.maven-publish")
+			plugin("org.gradle.signing")
 		}
-	}
 
-	val dokkaJar by tasks.registering(Jar::class) {
-		group = "build"
-		description = "Assembles the Kotlin docs with Dokka"
-		archiveClassifier.set("javadoc")
-		from(tasks.named("dokkaJavadoc"))
-	}
+		val sourcesJar by tasks.registering(Jar::class) {
+			group = "build"
+			description = "Assembles the source code into a jar"
+			archiveClassifier.set("sources")
+			from(sourceSets.main.map { it.allSource })
+		}
 
-	artifacts {
-		archives(sourcesJar)
-		archives(dokkaJar)
-	}
-
-	signing {
-		val signingKey: String? by project
-		val signingKeyPassword: String? by project
-		useInMemoryPgpKeys(signingKey, signingKeyPassword)
-	}
-
-	publishing.publications.register<MavenPublication>("maven") {
-		artifactId = if (extra.has("artifactId")) extra["artifactId"] as String else project.name
-
-		from(components["java"])
-		artifact(sourcesJar)
-		artifact(dokkaJar)
-
-		signing.sign(this)
-
-		pom {
-			name.set("$groupId:$artifactId")
-			description.set("Easily manage test files and directories when testing with Spek!")
-			inceptionYear.set("2020")
-			url.set("https://github.com/$githubRepository")
-			ciManagement {
-				system.set("GitHub Actions")
-				url.set("https://github.com/$githubRepository/actions")
-			}
-			issueManagement {
-				system.set("GitHub Issues")
-				url.set("https://github.com/$githubRepository/issues")
-			}
-			developers {
-				developer {
-					name.set("Joshua Gleitze")
-					email.set("dev@joshuagleitze.de")
+		tasks.withType<DokkaTask> {
+			dokkaSourceSets.named("main") {
+				this.DokkaSourceSetID(if (extra.has("artifactId")) extra["artifactId"] as String else project.name)
+				sourceLink {
+					val projectPath = projectDir.absoluteFile.relativeTo(rootProject.projectDir.absoluteFile)
+					localDirectory.set(file("src/main/kotlin"))
+					remoteUrl.set(uri("https://github.com/$githubRepository/blob/$gitRef/$projectPath/src/main/kotlin").toURL())
+					remoteLineSuffix.set("#L")
 				}
 			}
-			scm {
-				connection.set("scm:git:https://github.com/$githubRepository.git")
-				developerConnection.set("scm:git:git://git@github.com:$githubRepository.git")
+		}
+
+		val dokkaJar by tasks.registering(Jar::class) {
+			group = "build"
+			description = "Assembles the Kotlin docs with Dokka"
+			archiveClassifier.set("javadoc")
+			from(tasks.named("dokkaJavadoc"))
+		}
+
+		artifacts {
+			archives(sourcesJar)
+			archives(dokkaJar)
+		}
+
+		signing {
+			val signingKey: String? by project
+			val signingKeyPassword: String? by project
+			useInMemoryPgpKeys(signingKey, signingKeyPassword)
+		}
+
+		publishing.publications.create<MavenPublication>("maven") {
+			artifactId = if (extra.has("artifactId")) extra["artifactId"] as String else project.name
+
+			from(components["java"])
+			artifact(sourcesJar)
+			artifact(dokkaJar)
+
+			signing.sign(this)
+
+			pom {
+				name.set("$groupId:$artifactId")
+				description.set("Easily manage test files and directories when testing with Spek!")
+				inceptionYear.set("2020")
 				url.set("https://github.com/$githubRepository")
-			}
-			licenses {
-				license {
-					name.set("MIT")
-					url.set("https://opensource.org/licenses/MIT")
-					distribution.set("repo")
+				ciManagement {
+					system.set("GitHub Actions")
+					url.set("https://github.com/$githubRepository/actions")
+				}
+				issueManagement {
+					system.set("GitHub Issues")
+					url.set("https://github.com/$githubRepository/issues")
+				}
+				developers {
+					developer {
+						name.set("Joshua Gleitze")
+						email.set("dev@joshuagleitze.de")
+					}
+				}
+				scm {
+					connection.set("scm:git:https://github.com/$githubRepository.git")
+					developerConnection.set("scm:git:git://git@github.com:$githubRepository.git")
+					url.set("https://github.com/$githubRepository")
+				}
+				licenses {
+					license {
+						name.set("MIT")
+						url.set("https://opensource.org/licenses/MIT")
+						distribution.set("repo")
+					}
 				}
 			}
 		}
-	}
 
-	val githubPackages = publishing.repositories.maven("https://maven.pkg.github.com/$githubRepository") {
-		name = "GitHubPackages"
-		credentials {
-			username = githubOwner
-			password = githubToken
+		val githubPackages = publishing.repositories.maven("https://maven.pkg.github.com/$githubRepository") {
+			name = "GitHubPackages"
+			credentials {
+				username = githubOwner
+				password = githubToken
+			}
 		}
+
+		val mavenCentral = nexusPublishing.repositories.sonatype {
+			username.set(ossrhUsername)
+			password.set(ossrhPassword)
+		}
+
+		val publishToGithub = tasks.named("publishAllPublicationsTo${githubPackages.name.capitalize()}Repository")
+		val publishToMavenCentral = tasks.named("publishTo${mavenCentral.name.capitalize()}")
+
+		tasks.register("release") {
+			group = "release"
+			description = "Releases the project to all remote repositories"
+			dependsOn(publishToGithub, publishToMavenCentral, rootProject.tasks.closeAndReleaseRepository)
+		}
+
+		rootProject.tasks.closeAndReleaseRepository { mustRunAfter(publishToMavenCentral) }
 	}
-
-	val mavenCentral = nexusPublishing.repositories.sonatype {
-		username.set(ossrhUsername)
-		password.set(ossrhPassword)
-	}
-
-	val publishToGithub = tasks.named("publishAllPublicationsTo${githubPackages.name.capitalize()}Repository")
-	val publishToMavenCentral = tasks.named("publishTo${mavenCentral.name.capitalize()}")
-
-	tasks.register("release") {
-		group = "release"
-		description = "Releases the project to all remote repositories"
-		dependsOn(publishToGithub, publishToMavenCentral, rootProject.tasks.closeAndReleaseRepository)
-	}
-
-	rootProject.tasks.closeAndReleaseRepository { mustRunAfter(publishToMavenCentral) }
 }
 
 val Project.isSnapshot get() = versionDetails.commitDistance != 0
