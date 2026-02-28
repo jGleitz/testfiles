92573d9943d98dc8798335f54b263ecd06b7804c
Joshua Gleitze <dev@joshuagleitze.de>
2026-02-28 18:15:54 +0100

    build: apply develocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
 rootProject.name = "testfiles"
 
+plugins {
+  id("com.gradle.develocity") version "4.3.2"
+}
+
 include("base", "kotest", "spek")
+
+develocity {
+  buildScan {
+    publishing.onlyIf { false }
+  }
+}
